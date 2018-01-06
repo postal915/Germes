d47ab42ee63dd9119193debeb37ace03f5ea6868
Stas Latyshev <postal915@gmail.com>
2018-01-06 18:01:23 +0200

    add class Station Station where passengers can get off or take specific kind of transport. Multiple stations compose route of the trip.
    2-25

---
+++ germes-model/src/main/java/com/github/postal915/germes/app/model/entity/geography/Station.java
@@ -0,0 +1,65 @@
+package com.github.postal915.germes.app.model.entity.geography;
+
+import com.github.postal915.germes.app.model.entity.base.AbstractEntity;
+import com.github.postal915.germes.app.model.entity.transport.TransportType;
+
+/**
+ * Station where passengers can get off or take specific kind
+ * of transport. Multiple stations compose route of the trip.
+ */
+
+public class Station extends AbstractEntity {
+
+    private City city;
+
+    private Address address;
+
+    /**
+     * (Optional) Phone of the inquiry office
+     */
+    private String phone;
+
+    private Coordinate coordinate;
+
+    private TransportType transportType;
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+
+    public TransportType getTransportType() {
+        return transportType;
+    }
+
+    public void setTransportType(TransportType transportType) {
+        this.transportType = transportType;
+    }
+}
