57ef9601f8c0325a24bab900b1be1c39b6054747
Stas Latyshev <postal915@gmail.com>
2018-01-06 17:31:39 +0200

    add enum TransportType Type of the station/transport/vehicle that we support in the application

---
+++ germes-model/src/main/java/com/github/postal915/germes/app/model/entity/transport/TransportType.java
@@ -0,0 +1,12 @@
+package com.github.postal915.germes.app.model.entity.transport;
+
+/**
+ * Type of the station/transport/vehicle
+ * that we support in the application
+ */
+
+public enum TransportType {
+
+    AUTO /*For example, bus*/, RAILWAY /* For example, train*/, AVIA /*For example, plain*/;
+
+}
