49d7130260974538055f9c44ed287718484e2992
Stas Latyshev <postal915@gmail.com>
2018-01-06 17:26:40 +0200

    add abstract class AbstractEntity Base class for all business entities add class Account Entity that encapsulates user of the application

---
+++ germes-model/src/main/java/com/github/postal915/germes/app/model/entity/person/Account.java
@@ -0,0 +1,11 @@
+package com.github.postal915.germes.app.model.entity.person;
+
+import com.github.postal915.germes.app.model.entity.base.AbstractEntity;
+
+/**
+ * Entity that encapsulate user of the application
+ */
+
+public class Account extends AbstractEntity {
+
+}
