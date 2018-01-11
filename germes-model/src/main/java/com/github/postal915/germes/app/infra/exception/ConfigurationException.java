2ad31ca4d8eee80d49b190298343e9177e346157
Stas Latyshev <postal915@gmail.com>
2018-01-11 17:11:42 +0200

    added class Signals about incorrect configuration settings/parameters

---
+++ germes-model/src/main/java/com/github/postal915/germes/app/infra/exception/ConfigurationException.java
@@ -0,0 +1,20 @@
+package com.github.postal915.germes.app.infra.exception;
+
+import com.github.postal915.germes.app.infra.exception.base.AppException;
+
+/**
+ * Signals about incorrect configuration settings/parameters
+ */
+
+public class ConfigurationException extends AppException {
+
+    public static final long serialVersionUID = -2177284893894040026L;
+
+    public ConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConfigurationException(String message) {
+        super(message);
+    }
+}
