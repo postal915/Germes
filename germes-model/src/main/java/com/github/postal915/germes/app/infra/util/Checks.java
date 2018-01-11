3feb128005ad5924383cc08401c1a0c1164b2ae6
Stas Latyshev <postal915@gmail.com>
2018-01-11 18:48:42 +0200

    added class Contains common check routines

---
+++ germes-model/src/main/java/com/github/postal915/germes/app/infra/util/Checks.java
@@ -0,0 +1,27 @@
+package com.github.postal915.germes.app.infra.util;
+
+import com.github.postal915.germes.app.infra.exception.flow.InvalidParameterException;
+
+/**
+ * Contains common check routines
+ */
+
+public class Checks {
+
+    private Checks() {
+    }
+
+    /**
+     * Verifies that specified check passed and throws exception otherwise
+     * @param check
+     * @param message
+     * @throws InvalidParameterException
+     */
+    public static void checkParameter(boolean check, String message)
+            throws InvalidParameterException {
+        if (!check) {
+            throw new InvalidParameterException(message);
+        }
+    }
+}
+
