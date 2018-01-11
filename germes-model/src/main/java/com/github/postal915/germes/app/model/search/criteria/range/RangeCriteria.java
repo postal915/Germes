59fc090b455cc0e532868135efb8c9f017b685c6
Stas Latyshev <postal915@gmail.com>
2018-01-11 17:19:02 +0200

    updated method added check for exceptions

--- germes-model/src/main/java/com/github/postal915/germes/app/model/search/criteria/range/RangeCriteria.java
+++ germes-model/src/main/java/com/github/postal915/germes/app/model/search/criteria/range/RangeCriteria.java
@@ -1,5 +1,7 @@
 package com.github.postal915.germes.app.model.search.criteria.range;
 
+import com.github.postal915.germes.app.infra.exception.flow.InvalidParameterException;
+
 /**
  * Pagination parameters for data retrieval operations
  */
@@ -17,6 +19,14 @@ public class RangeCriteria {
     private final int rowCount;
 
     public RangeCriteria(final int page, final int rowCount) {
+
+        if (page < 0) {
+            throw new InvalidParameterException("Incorrect page index: " + page);
+        }
+        if (rowCount < 0) {
+            throw new InvalidParameterException("Incorrect row count: " + rowCount);
+        }
+
         this.page = page;
         this.rowCount = rowCount;
     }
