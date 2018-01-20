2b96ee9a9d0d219a51f184410ea9bc0c12146f6f
Stas Latyshev <postal915@gmail.com>
2018-01-20 11:25:27 +0200

    update class AppException added new constructor update class ConfigurationException added new constructor

--- germes-model/src/main/java/com/github/postal915/germes/app/infra/exception/base/AppException.java
+++ germes-model/src/main/java/com/github/postal915/germes/app/infra/exception/base/AppException.java
@@ -15,4 +15,8 @@ public abstract class AppException extends RuntimeException {
     public AppException(String message) {
         super(message);
     }
+
+    public AppException(Throwable throwable) {
+        super(throwable);
+    }
 }
