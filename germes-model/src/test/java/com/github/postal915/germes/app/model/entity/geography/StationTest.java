16f29ff5a0947d2b822411e23e90d294e1a3b020
Stas Latyshev <postal915@gmail.com>
2018-01-29 20:20:20 +0200

    added tests for the class StationTest

---
+++ germes-model/src/test/java/com/github/postal915/germes/app/model/entity/geography/StationTest.java
@@ -0,0 +1,37 @@
+package com.github.postal915.germes.app.model.entity.geography;
+
+import com.github.postal915.germes.app.model.entity.transport.TransportType;
+import com.github.postal915.germes.app.model.search.criteria.StationCriteria;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Verifies functionality of the {@link Station} domain entity
+ */
+public class StationTest {
+
+    @Test(expected = NullPointerException.class)
+    public void testMatchCriteriaNotInitialized() {
+        City city = new City("Odessa");
+        Station station = new Station(city, TransportType.AUTO);
+
+        station.match(null);
+    }
+
+    @Test
+    public void testMatchByNameSuccess() {
+        City city = new City("Odessa");
+        Station station = new Station(city, TransportType.AUTO);
+
+        assertTrue(station.match(StationCriteria.byName("Odessa")));
+    }
+
+    @Test
+    public void testMatchByNameNotFound() {
+        City city = new City("Odessa");
+        Station station = new Station(city, TransportType.AUTO);
+
+        assertFalse(station.match(StationCriteria.byName("Kiev")));
+    }
+}
