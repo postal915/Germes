4b3915c9b2518232ba920296f9439483c8caf436
Stas Latyshev <postal915@gmail.com>
2018-01-29 16:59:56 +0200

    updated added new methods

--- germes-service/src/main/java/com/github/postal915/germes/app/service/GeographicService.java
+++ germes-service/src/main/java/com/github/postal915/germes/app/service/GeographicService.java
@@ -1,20 +1,41 @@
 package com.github.postal915.germes.app.service;
 
 import com.github.postal915.germes.app.model.entity.geography.City;
+import com.github.postal915.germes.app.model.entity.geography.Station;
+import com.github.postal915.germes.app.model.search.criteria.StationCriteria;
+import com.github.postal915.germes.app.model.search.criteria.range.RangeCriteria;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
- * Entity point to perform operations
- * over geographic entities
+ * Entity point to perform operations over geographic entities
  */
 
 public interface GeographicService {
 
     /**
      * Returns list of existing cities
+     *
+     * @return
      */
     List<City> findCities();
+
+    /**
+     * Returns city with specified identified. If no city is found then empty optional is returned
+     *
+     * @param id
+     * @return
+     */
+    Optional<City> findCitiById(int id);
+
+    /**
+     * Returns all the stations that match specified criteria
+     * @param criteria
+     * @param rangeCriteria
+     * @return
+     */
+    List<Station> searchStations(StationCriteria criteria, RangeCriteria rangeCriteria);
 
     /**
      * Saves specified city instance
