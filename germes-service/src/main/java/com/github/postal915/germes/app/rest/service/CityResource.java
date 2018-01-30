4bd69e4b3051c1200ef1561b0d48f173dd7d3132
Stas Latyshev <postal915@gmail.com>
2018-01-30 23:13:56 +0200

    updated class

--- germes-service/src/main/java/com/github/postal915/germes/app/rest/service/CityResource.java
+++ germes-service/src/main/java/com/github/postal915/germes/app/rest/service/CityResource.java
@@ -1,22 +1,89 @@
 package com.github.postal915.germes.app.rest.service;
 
-import com.google.common.collect.Lists;
+import com.github.postal915.germes.app.model.entity.geography.City;
+import com.github.postal915.germes.app.model.entity.transport.TransportType;
+import com.github.postal915.germes.app.rest.dto.CityDTO;
+import com.github.postal915.germes.app.rest.service.base.BaseResource;
+import com.github.postal915.germes.app.service.GeographicService;
+import com.github.postal915.germes.app.service.impl.GeographicServiceImpl;
+import com.github.postal915.germes.app.service.transform.Transformer;
+import com.github.postal915.germes.app.service.transform.impl.SimpleDTOTransformer;
+import org.apache.commons.lang3.math.NumberUtils;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Path("cities")
 /**
  * {@link CityResource} is REST web-service that handles city-related requests
  */
-public class CityResource {
+public class CityResource extends BaseResource {
 
+    /**
+     * Underlying source of data
+     */
+    private final GeographicService service;
+
+    /**
+     * DTO <-> Entity transformer
+     */
+    private final Transformer transformer;
+
+    public CityResource() {
+        transformer = new SimpleDTOTransformer();
+        service = new GeographicServiceImpl();
+        City city = new City("Odessa");
+        city.addStation(TransportType.AUTO);
+        service.saveCity(city);
+    }
+
+    /**
+     * Returns all the existing cities
+     *
+     * @return
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<String> findCities() {
-        return Lists.newArrayList("Odessa", "Kiev");
+    public List<CityDTO> findCities() {
+        return service.findCities().stream().map(city -> transformer.transform(city, CityDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Saves new city instance
+     *
+     * @param cityDTO
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void saveCity(CityDTO cityDTO) {
+        service.saveCity(transformer.unTransform(cityDTO, City.class));
+    }
+
+    /**
+     * Returns city with specified identifier
+     *
+     * @param cityId
+     * @return
+     */
+    @Path("/{cityId}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findCityById(@PathParam("cityId") final String cityId) {
+        if (!NumberUtils.isNumber(cityId)) {
+            return BAD_REQUEST;
+        }
+
+        Optional<City> city = service.findCityById(NumberUtils.toInt(cityId));
+
+        if (!city.isPresent()) {
+            return NOT_FOUND;
+        }
+
+        return ok(transformer.transform(city.get(), CityDTO.class));
     }
 }
