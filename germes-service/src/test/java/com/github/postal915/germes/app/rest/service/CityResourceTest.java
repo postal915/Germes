cae479cdbadb430a3a02b9b626786d235a4fcf19
Stas Latyshev <postal915@gmail.com>
2018-01-31 20:11:17 +0200

    updated tests

--- germes-service/src/test/java/com/github/postal915/germes/app/rest/service/CityResourceTest.java
+++ germes-service/src/test/java/com/github/postal915/germes/app/rest/service/CityResourceTest.java
@@ -1,19 +1,25 @@
 package com.github.postal915.germes.app.rest.service;
 
+import com.github.postal915.germes.app.rest.dto.CityDTO;
 import com.github.postal915.germes.app.rest.service.config.JerseyConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
+
+
 
 /**
  * {@link CityResourceTest} is integration test that verifies
  * {@link CityResource}
  */
-
 public class CityResourceTest extends JerseyTest {
 
     @Override
@@ -23,10 +29,44 @@ public class CityResourceTest extends JerseyTest {
 
     @Test
     public void testFindCitiesSuccess() {
-        List<?> cities = target("cities").request().get(List.class);
-
+        List<Map<String, String>> cities = target("cities").request().get(List.class);
         assertNotNull(cities);
-        assertTrue(cities.contains("Odessa"));
-        assertTrue(cities.contains("Kiev"));
+        assertEquals(cities.size(), 1);
+
+        Map<String, String> city = cities.get(0);
+        assertEquals(city.get("name"), "Odessa");
+    }
+
+    @Test
+    public void testFindCityByIdSuccess() {
+        CityDTO city = target("cities/1").request().get(CityDTO.class);
+
+        assertNotNull(city);
+        assertEquals(city.getId(), 1);
+        assertEquals(city.getName(), "Odessa");
+    }
+
+    @Test
+    public void testFindCityByIdNotFound() {
+        Response response = target("cities/2").request().get(Response.class);
+
+        assertNotNull(response);
+        assertEquals(response.getStatus(), Response.Status.NOT_FOUND.getStatusCode());
+
+    }
+
+    @Test
+    public void testFindCityBuIdInvalidId() {
+        Response response = target("cities/aaab").request().get(Response.class);
+        assertNotNull(response);
+        assertEquals(response.getStatus(), Response.Status.BAD_REQUEST.getStatusCode());
+    }
+
+    @Test
+    public void testSaveCitySuccess() {
+        CityDTO city = new CityDTO();
+        city.setName("Kiev");
+        Response response = target("cities").request().post(Entity.entity(city, MediaType.APPLICATION_JSON));
+        assertEquals(response.getStatus(), Response.Status.NO_CONTENT.getStatusCode());
     }
 }
