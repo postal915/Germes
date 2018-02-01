d14e50b83a1cb89d22454551e10e15dad39affee
Stas Latyshev <postal915@gmail.com>
2018-02-01 09:47:12 +0200

    added class GlobalExceptionHandler Global Jersey handler that catches any Exception-based errors

---
+++ germes-service/src/main/java/com/github/postal915/germes/app/rest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.github.postal915.germes.app.rest.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Global Jersey handler that catches any Exception-based errors
+ */
+@Provider
+public class GlobalExceptionHandler implements ExceptionMapper<Exception> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    private final Response SERVER_ERROR;
+
+    public GlobalExceptionHandler() {
+        SERVER_ERROR = Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @Override
+    public Response toResponse(Exception e) {
+        LOGGER.error(e.getMessage(), e);
+
+        return SERVER_ERROR;
+    }
+}
