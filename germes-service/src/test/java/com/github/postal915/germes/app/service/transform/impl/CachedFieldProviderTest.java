8dc1d1305587568350c315fed33d81e271f61dcf
Stas Latyshev <postal915@gmail.com>
2018-02-02 19:31:27 +0200

    update test added new method

--- germes-service/src/test/java/com/github/postal915/germes/app/service/transform/impl/CachedFieldProviderTest.java
+++ germes-service/src/test/java/com/github/postal915/germes/app/service/transform/impl/CachedFieldProviderTest.java
@@ -1,15 +1,24 @@
 package com.github.postal915.germes.app.service.transform.impl;
 
+import com.github.postal915.germes.app.infra.util.ReflectionUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * Verifies functionality of the {@link CachedFieldProvider} unit
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ReflectionUtil.class)
 public class CachedFieldProviderTest {
 
     private FieldProvider provider;
@@ -29,6 +38,20 @@ public class CachedFieldProviderTest {
     @Test
     public void testGetFieldNamesCachedSuccess() {
         List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+        List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
+        assertFalse(fields.isEmpty());
+        assertEquals(fields, fields2);
+    }
+
+    @Test
+    public void testGetFieldNamesAreCached() {
+        List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+
+        PowerMockito.mockStatic(ReflectionUtil.class);
+        when(ReflectionUtil.findSimilarFields(Source.class, Destination.class)).thenReturn(Collections.emptyList());
+
+        assertTrue(ReflectionUtil.findSimilarFields(Source.class, Destination.class).isEmpty());
+
         List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
         assertFalse(fields.isEmpty());
         assertEquals(fields, fields2);
