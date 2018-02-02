b462f28f557ff4c32bd98cdd4ed54cb4b6e668be
Stas Latyshev <postal915@gmail.com>
2018-02-02 16:18:00 +0200

    added test CachedFieldProviderTest

---
+++ germes-service/src/test/java/com/github/postal915/germes/app/service/transform/impl/CachedFieldProviderTest.java
@@ -0,0 +1,44 @@
+package com.github.postal915.germes.app.service.transform.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Verifies functionality of the {@link CachedFieldProvider} unit
+ */
+public class CachedFieldProviderTest {
+
+    private FieldProvider provider;
+
+    @Before
+    public void setup() {
+        provider = new CachedFieldProvider();
+    }
+
+    @Test
+    public void testGetFieldNamesSuccess() {
+        List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+        assertFalse(fields.isEmpty());
+        assertTrue(fields.contains("value"));
+    }
+
+    @Test
+    public void testGetFieldNamesCachedSuccess() {
+        List<String> fields = provider.getFieldNames(Source.class, Destination.class);
+        List<String> fields2 = provider.getFieldNames(Source.class, Destination.class);
+        assertFalse(fields.isEmpty());
+        assertEquals(fields, fields2);
+    }
+}
+
+class Source {
+    int value;
+}
+
+class Destination {
+    int value;
+}
