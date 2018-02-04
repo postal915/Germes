f66e22ef70d15ed683b1cc9a520b21ab43ec4aaf
Stas Latyshev <postal915@gmail.com>
2018-02-04 11:23:36 +0200

    updated class add annotation

--- germes-model/src/main/java/com/github/postal915/germes/app/model/entity/person/Account.java
+++ germes-model/src/main/java/com/github/postal915/germes/app/model/entity/person/Account.java
@@ -2,10 +2,14 @@ package com.github.postal915.germes.app.model.entity.person;
 
 import com.github.postal915.germes.app.model.entity.base.AbstractEntity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Entity that encapsulate user of the application
  */
-
+@Table(name = "ACCOUNT")
+@Entity
 public class Account extends AbstractEntity {
 
 }
