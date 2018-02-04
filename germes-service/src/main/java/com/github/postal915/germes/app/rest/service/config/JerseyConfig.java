d9fefd252b053208345ea99ad6c62109043bb027
Stas Latyshev <postal915@gmail.com>
2018-02-04 10:21:31 +0200

    updated method

--- germes-service/src/main/java/com/github/postal915/germes/app/rest/service/config/JerseyConfig.java
+++ germes-service/src/main/java/com/github/postal915/germes/app/rest/service/config/JerseyConfig.java
@@ -1,5 +1,6 @@
 package com.github.postal915.germes.app.rest.service.config;
 
+import com.github.postal915.germes.app.config.ComponentFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
@@ -12,6 +13,7 @@ import javax.ws.rs.ApplicationPath;
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
+        super(ComponentFeature.class);
         packages("com.github.postal915.germes.app.rest");
     }
 }
