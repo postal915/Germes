79f26fbdcc983dbeab5645cf5c97df5a4ea3f3bf
Stas Latyshev <postal915@gmail.com>
2018-02-25 19:37:32 +0200

    updated maven and gradle file update project version updated class HibernateCityRepository add CRUD operation Hibernate

--- germes-persistence/src/main/java/com/github/postal915/germes/app/persistence/repository/hibernate/HibernateCityRepository.java
+++ germes-persistence/src/main/java/com/github/postal915/germes/app/persistence/repository/hibernate/HibernateCityRepository.java
@@ -1,16 +1,51 @@
 package com.github.postal915.germes.app.persistence.repository.hibernate;
 
+import com.github.postal915.germes.app.model.entity.geography.City;
 import com.github.postal915.germes.app.persistence.hibernate.SessionFactoryBuilder;
+import com.github.postal915.germes.app.persistence.repository.CityRepository;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import javax.inject.Inject;
+import java.util.List;
 
-public class HibernateCityRepository {
+public class HibernateCityRepository implements CityRepository {
 
     private final SessionFactory sessionFactory;
 
     @Inject
     public HibernateCityRepository(SessionFactoryBuilder builder) {
         sessionFactory = builder.getSessionFactory();
+    }
+
+    @Override
+    public void save(City city) {
+        try (Session session = sessionFactory.openSession()) {
+            session.saveOrUpdate(city);
+        }
+    }
+
+    @Override
+    public City findById(int cityId) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(City.class, cityId);
+        }
+    }
+
+    @Override
+    public void delete(int cityId) {
+        try (Session session = sessionFactory.openSession()) {
+            City city = session.get(City.class, cityId);
+            if (city != null) {
+                session.delete(city);
+            }
+        }
+    }
+
+    @Override
+    public List<City> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createCriteria(City.class).list();
+        }
     }
 }
