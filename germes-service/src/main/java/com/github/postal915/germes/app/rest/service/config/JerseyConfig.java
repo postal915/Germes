7f83365dc72690d0ec75bf86abe9629fa519a49a
Stas Latyshev <postal915@gmail.com>
2018-03-22 12:26:41 +0200

    72-138

--- germes-service/src/main/java/com/github/postal915/germes/app/rest/service/config/JerseyConfig.java
+++ germes-service/src/main/java/com/github/postal915/germes/app/rest/service/config/JerseyConfig.java
@@ -1,6 +1,9 @@
 package com.github.postal915.germes.app.rest.service.config;
 
 import com.github.postal915.germes.app.config.ComponentFeature;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.ApplicationPath;
@@ -14,5 +17,20 @@ public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         super(ComponentFeature.class);
         packages("com.github.postal915.germes.app.rest");
+
+        initBeanConfig();
+
+        register(ApiListingResource.class);
+        register(SwaggerSerializers.class);
+    }
+
+    private void initBeanConfig() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/");
+        beanConfig.setResourcePackage("com.github.postal915.germes.app.rest.service");
+        beanConfig.setScan(true);
     }
 }
